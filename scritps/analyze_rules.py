87cfb3a82caebab5b6de5ef2a331010dabeded4a
Christian Scano <cris.scano@outlook.it>
2024-04-05 19:15:18 +0200

    Added graphs

---
+++ scritps/analyze_rules.py
@@ -0,0 +1,147 @@
+import toml
+import os
+import sys
+import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
+import seaborn.objects as so
+import numpy as np
+import pandas as pd
+import json
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from sklearn.preprocessing import minmax_scale
+
+
+def analyze_weights(
+    model_name,
+    model_l1,
+    model_l2,
+    crs_ids,
+    crs_weights,
+    figure_path,
+    legend_fontsize  = 13,
+    axis_labels_size = 16,
+    tick_labels_size = 14
+):
+
+    model_l1_weights = model_l1.coef_.flatten()
+    model_l2_weights = model_l2.coef_.flatten()
+
+    modsec_weights = np.array([int(crs_weights[rule]) for rule in crs_ids])
+    modsec_weights = np.append(modsec_weights, 0)
+    modsec_weights = minmax_scale(modsec_weights, feature_range=(0, model_l1_weights.max()))
+
+    fig, axs = plt.subplots(1, 1)
+    
+    df_plot = pd.DataFrame(
+        {
+            'rules': crs_ids * 3,
+            'weight': modsec_weights.tolist()[:-1] + model_l1_weights.tolist() + model_l2_weights.tolist(),
+            'type': ['ModSec'] * len(crs_ids) + 
+                    [f'{model_name} - L1'] * len(crs_ids) + 
+                    [f'{model_name} - L2'] * len(crs_ids)
+        }
+    )
+
+    _ = so.Plot(
+        df_plot, 
+        x     = 'rules',
+        y     = 'weight',
+        color = 'type'
+    ) \
+        .add(so.Bar()) \
+        .scale(color=['#aedc41', '#81b8ef', '#fe6d73']) \
+        .on(axs) \
+        .plot()
+    
+    legend = fig.legends.pop(0)
+
+    axs.set_xticklabels(
+        [rule[3:] for rule in crs_ids], 
+        rotation=75,
+        ha='right',
+        rotation_mode='anchor'
+    )
+    axs.legend(
+        legend.legendHandles, 
+        [t.get_text() for t in legend.texts], 
+        loc='lower right', 
+        fancybox=True,
+        shadow=False,
+        fontsize=legend_fontsize
+    )
+    
+    axs.set_xlabel('CRS SQLi Rules', fontsize=axis_labels_size, labelpad=10)
+    axs.set_ylabel('Weight', fontsize=axis_labels_size, labelpad=10)
+    axs.set_xmargin(0.05)
+    axs.set_ymargin(0.3)
+    axs.xaxis.set_tick_params(labelsize=tick_labels_size)
+    axs.yaxis.set_tick_params(labelsize=tick_labels_size)
+    
+    axs.grid(visible=True, axis='both', color='gray', linestyle='dotted')
+    
+    fig.set_size_inches(16, 6)
+    fig.tight_layout()
+    fig.savefig(
+        os.path.join( 
+            figure_path,
+            '{}_weights_comp.pdf'.format(model_name.lower())
+        ), 
+        dpi         = 600,
+        format      = 'pdf',
+        bbox_inches = "tight"
+    )
+
+
+if __name__ == '__main__':
+    settings         = toml.load('config.toml')
+    crs_ids_path     = settings['crs_ids_path']
+    crs_weiths_path  = settings['crs_weights_path']
+    models_path      = settings['models_path']
+    figures_path     = settings['figures_path']
+    pl               = 4
+
+    with open(crs_ids_path) as file:
+        crs_ids = sorted(json.load(file)['rules_ids'])
+    
+    with open(crs_weiths_path) as file:
+        weights = json.load(file)['weights']
+    
+    # Linear SVC
+    model_name = 'linear_svc_pl{}_l1.joblib'.format(pl)
+    model_l1      = joblib.load(
+        os.path.join(models_path, model_name)
+    )
+    model_name = 'linear_svc_pl{}_l2.joblib'.format(pl)
+    model_l2      = joblib.load(
+        os.path.join(models_path, model_name)
+    )
+
+    analyze_weights(
+        'SVC',
+        model_l1,
+        model_l2,
+        crs_ids,
+        weights,
+        figures_path
+    )
+
+    # Logistic Regression
+    model_name = 'log_reg_pl{}_l1.joblib'.format(pl)
+    model_l1      = joblib.load(
+        os.path.join(models_path, model_name)
+    )
+    model_name = 'log_reg_pl{}_l2.joblib'.format(pl)
+    model_l2      = joblib.load(
+        os.path.join(models_path, model_name)
+    )
+
+    analyze_weights(
+        'Logistic Regression',
+        model_l1,
+        model_l2,
+        crs_ids,
+        weights,
+        figures_path
+    )
